a9317e193d06c8d7b4fac5a19de9f0d84c0d1d28
Lauras5 <idlaurasalcido@gmail.com>
2021-03-31 22:58:47 -0700

    added lifetimes

--- src/http/request.rs
+++ src/http/request.rs
@@ -5,22 +5,22 @@ use std::fmt::{Result as FmtResult, Formatter, Display, Debug};
 use std::str;
 use std::str::Utf8Error;
 
-pub struct Request {
-    path: String,
-    query_string: Option<String>,
+pub struct Request<'buf> { // lifetime for buffer
+    path: &'buf str,
+    query_string: Option<&'buf str>,
     method: Method,
 }
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
     // convert byte slice into string slice
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buf: &'buf [u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
 
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
@@ -28,7 +28,19 @@ impl TryFrom<&[u8]> for Request {
         }
 
         let method: Method = method.parse()?;
-        unimplemented!();
+
+        let mut query_string = None; // since optional
+
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i];
+        }
+
+        Ok(Self {
+            path,
+            query_string,
+            method
+        })
     }
 }
 
